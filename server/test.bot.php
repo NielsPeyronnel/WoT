01b861a33838d9f15bcbc185c4313645b03bb2aa
Niels PEYRONNEL <npeyronnel@liveaction.com>
2019-03-20 17:50:01 +0100

    client & server implementation

---
+++ server/test.bot.php
@@ -0,0 +1,42 @@
+#!/usr/bin/php -q
+
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: niels
+ * Date: 19/03/2019
+ * Time: 10:38
+ */
+
+
+// inclusion de la classe
+require 'phpwebsocket/websocket.class.php';
+
+
+// extension de la classe websocket
+class ChatBot extends WebSocket {
+    function process($user, $msg){
+        $this->say("<  ".$msg);
+        switch ($msg){
+            case 'hello':
+                $this->send($user->socket,"Bonjour");
+                break;
+            case 'date':
+                $this->send($user->socket,"Nous sommes le ".date("d/m/y"));
+                break;
+            case 'bye':
+            case 'ciao':
+                $this->send($user->socket,"Au revoir !");
+                $this->disconnect($user->socket);
+                break;
+            default:
+                $this->send($user->socket,"Je n'ai pas compris");
+                break;
+        }
+    }
+}
+
+$master = new ChatBot("0.0.0.0",1337);
+
+?>
+
---
+++ server/test.echo.php
@@ -0,0 +1,6 @@
+#!/usr/bin/php -q
+
+<?php
+    require "phpwebsocket/websocket.class.php";
+    $master = new WebSocket("0.0.0.0",1337);
+?>
