8026d8413fbf79813c08ad0546f451537be946b2
Niels PEYRONNEL <npeyronnel@liveaction.com>
2019-03-22 21:27:04 +0100

    update server

--- server/server_tag.php
+++ server/server_tag.php
@@ -8,26 +8,6 @@
 
 require './phpwebsocket/websocket.class.php';
 
-$servername = "localhost";
-$username = "niels";
-$password = "niteversion";
-$databse ="phpmyadmin";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password,$databse);
-
-// Check connection
-if ($conn->connect_error) {
-    die("[".date("H:i:s")."] --{MYSQL}-- Connection echec (" . $conn->connect_error.")\n\n");
-}
-
-echo "[".date("H:i:s")."] --{MYSQL}-- Connection établie\n\n";
-
-$query = "SELECT * FROM Event";
-$result = $conn->query($query);
-$row = $result->num_rows;
-
-
 class event extends WebSocket {
 
     function process($user, $msg){
@@ -36,6 +16,11 @@ class event extends WebSocket {
         $password = "niteversion";
         $databse ="phpmyadmin";
         $conn = new mysqli($servername, $username, $password,$databse);
+        if ($conn->connect_error) {
+            die("[".date("H:i:s")."] --{MYSQL}-- Connection echec (" . $conn->connect_error.")\n\n");
+        }
+
+        echo "[".date("H:i:s")."] --{MYSQL}-- Connection établie\n\n";
         $query =null;
         $query = "SELECT * FROM Event";
         $result = $conn->query($query);
