e58fa3898dbfd3695a0ca13b57de00ab32638ca8
Niels PEYRONNEL <npeyronnel@liveaction.com>
2019-03-22 19:16:37 +0100

    update server

--- server/server_tag.php
+++ server/server_tag.php
@@ -23,7 +23,7 @@ echo "[".date("H:i:s")."] --{MYSQL}-- Connection établie\n\n";
 
 $nb_event = "SELECT id FROM `Event`";
 $result = $conn->query($nb_event);
-echo "[".date("H:i:s")."] --{MYSQL}-- Résultat requête  ". $result->num_rows ."\n\n";
+echo $result;
 
 class event extends WebSocket {
     function process($user){
