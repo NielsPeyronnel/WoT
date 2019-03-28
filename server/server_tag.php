81f130d2f6b48d26601e965de365a4129bf20c7a
Niels PEYRONNEL <npeyronnel@liveaction.com>
2019-03-28 16:41:47 +0100

    update server

--- server/server_tag.php
+++ server/server_tag.php
@@ -56,7 +56,7 @@ class event extends WebSocket {
             case 'onloadEvent':
                 $query_event = "SELECT * FROM Event";
                 $result = $this->conn->query($query_event);
-                $table = $result->fetch_array();
+                $table = $result->fetch_all();
                 $array = array('type' => 'onload_event', 'data' => $table);
                 $this->send($user->socket,json_encode($array));
                 break;
