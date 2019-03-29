50078cfc2d7e40f71199dfd200dee45aa5f278dc
Niels PEYRONNEL <npeyronnel@liveaction.com>
2019-03-29 12:11:16 +0100

    update server

--- server/server_tag.php
+++ server/server_tag.php
@@ -35,6 +35,7 @@ class event extends WebSocket {
     }
 
     function process($user, $obj){
+        $obj = substr($obj, 0, strpos($obj, "}"));
         $msg = json_decode($obj,TRUE);
         echo"\n\n[".date("H:i:s")."] --{DEBUT DEBUG MESSAGE}-- \n";
         print_r($msg);
